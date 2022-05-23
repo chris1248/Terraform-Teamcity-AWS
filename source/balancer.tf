5dd1620ae8f3ee7da7c382f6b02eedabf367096d
Chris Johnson <cjohnson@telesign.com>
2022-05-23 12:47:49 -0600

    Add a front end load balancer to be publicly exposed.

---
+++ source/balancer.tf
@@ -0,0 +1,40 @@
+# Application Load Balancer
+resource "aws_lb" "front_end" {
+  name            = var.name
+  subnets         = [aws_subnet.private.id, aws_subnet.public.id]
+  security_groups = [aws_security_group.teamcity.id]
+  internal        = false
+  load_balancer_type = "application"
+
+  tags = merge(var.tags, {Name = "TeamCity"} )
+}
+
+resource "aws_lb_target_group" "targets" {
+  name        = var.name
+  port        = var.app_port
+  protocol    = "HTTPS"
+  target_type = "ip"
+  vpc_id      = aws_vpc.main.id
+
+  tags = merge(var.tags, {Name = "TeamCity"} )
+}
+
+variable "ports" {
+    type = list(number)
+    default = [8443, 443]
+}
+
+resource "aws_alb_listener" "http_front_end" {
+  count = length(var.ports)
+  load_balancer_arn = aws_lb.front_end.id
+  port              = var.ports[count.index]
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+
+  depends_on = [aws_lb_target_group.targets, aws_lb.front_end]
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.targets.arn
+  }
+  tags = merge(var.tags, {Name = "TeamCity"} )
+}
